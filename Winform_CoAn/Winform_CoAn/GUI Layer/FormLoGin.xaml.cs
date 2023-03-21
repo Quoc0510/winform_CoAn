2bcedab04f1ab51d76d0978e540138b0ddc376aa
LAPTOP-6BJ6USP1\ASUS <quoc76087@gmail.com>
2023-03-21 11:53:14 +0700

    UI quoc

---
+++ Winform_CoAn/Winform_CoAn/GUI Layer/FormLoGin.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Winform_CoAn.GUI_Layer
+{
+    /// <summary>
+    /// Interaction logic for FormLoGin.xaml
+    /// </summary>
+    public partial class FormLoGin : Window
+    {
+        public FormLoGin()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FormQuanLi formQuanLi = new FormQuanLi();
+            formQuanLi.Show();
+        }
+    }
+}
---
+++ Winform_CoAn/Winform_CoAn/GUI Layer/FormQuanLi.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Winform_CoAn.GUI_Layer
+{
+    /// <summary>
+    /// Interaction logic for FormQuanLi.xaml
+    /// </summary>
+    public partial class FormQuanLi : Window
+    {
+        public FormQuanLi()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FormQuanLi formQuanLi = new FormQuanLi();
+            formQuanLi.Show();
+        }
+    }
+}
